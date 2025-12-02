15178b2c916fbf4ac70d3c099d7369b238abeacb
nxtnilesh <nxtnilesh@gmail.com>
2025-12-02 16:20:14 +0530

    ui : ui imporvemment

--- src/App.jsx
+++ src/App.jsx
@@ -43,7 +43,7 @@ function App() {
         <ul className="bg-gray-300 p-3">
           {user.map((u) => (
             <div className="bg-green-50 rounded-2xl p-4 m-2 flex justify-between">
-              <div className="flex">
+              <div className="flex items-center">
                 <img src={u.image} alt="error" className="rounded-full h-11" />
                 <div className="">
                   <li key={u.id}>{u.firstName}</li>
@@ -61,10 +61,10 @@ function App() {
             </div>
           ))}
         </ul>
-        <div>
-          <button className="bg-blue-300" onClick={() => setPage((p) => p - 1)}>Previous</button>
+        <div className="flex items-center justify-center m-5 gap-4">
+          <button className="bg-blue-300 rounded-sm cursor-pointer p-2" onClick={() => setPage((p) => p - 1)}>Previous</button>
           <span>{page}</span>
-          <button className="bg-blue-300" onClick={() => setPage((p) => p + 1)}>Next</button>
+          <button className="bg-blue-300 rounded-sm cursor-pointer p-2" onClick={() => setPage((p) => p + 1)}>Next</button>
         </div>
       </div>
     </>
